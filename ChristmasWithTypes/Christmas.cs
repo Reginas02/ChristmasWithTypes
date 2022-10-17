9de89ce2efeda3bf09463c393b662232316fc6e3
Regina Naslund <rnaslund66@gmail.com>
2022-10-17 07:21:16 -0700

    inital commit

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -4,11 +4,11 @@ namespace ChristmasWithTypes
     public class Christmas
     {
         public string[] Presents;
-        public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public string Sant { get; set; }
+        public int? TreeHeight { get; set; } //DONE Make the Height property nullable
 
-        //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        //DONE Make the property, "Day", type enum
+        public enum Day { Sunday, Monday, Tuesday, Wendsday, Thursday, Friday, Saturday };
 
     }
 }
